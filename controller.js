dfe5d29904292f61d5476bf31d2843c01a1a2f2e
Omar Yusuf D. Tagayan <omaryusuftagayan@gmail.com>
2021-09-08 19:22:38 +0800

    initial commit

---
+++ controller.js
@@ -0,0 +1,22 @@
+const html = `
+<div
+ style="display:flex;padding:1rem;align-content:center;justify-content:center;"
+>
+  <h6 style="font-family:Arial;font-weight:bold;color:#FFFFFF;">Controller File</h6>
+</div>
+<script>
+  addEventListener("message", e=>{
+    if(e.source !== parent) return;
+    console.log("controller:")
+    console.log(e.data);
+  })
+</script>
+`;
+
+reearth.ui.show(html);
+reearth.on("update", () => {
+  reearth.ui.postMessage(reearth.block);
+});
+reearth.on("message", (msg) => {
+  console.log("message received:", msg);
+});
